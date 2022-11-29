ad029a40c33b531a26923f27052bbe42e05c7cfe
Rukorio-Vanessa <blwwbl55@gmail.com>
2022-11-30 00:27:32 +0300

    Completed assignment

---
+++ index.js
@@ -0,0 +1,8 @@
+//arrow function 'divide' that divides 2000 by 100
+const  divide = () => 2000 / 100
+
+//arrow function 'square' with a parameter(x) that squares x
+const square = x => x ** 2
+
+//arow function 'add' with 2 parameters (a,b) that adds them together
+const add = (a, b) => a + b
